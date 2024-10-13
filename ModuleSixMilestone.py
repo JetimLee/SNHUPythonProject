9e3b0ed4b566e73f8939d5b5436aa3391aa2d871
Gavin Coulson <gavincoulson@Gavins-MacBook-Pro.local>
2024-10-13 14:46:52 -0600

    using provided dictionary

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -1,15 +1,6 @@
+# Gavin Coulson
 rooms = {
-    "Prison Tower": {"South": "Vault", "East": "Shuttle Landing Platform"},
-    "Vault": {"North": "Prison Tower", "East": "Grand Training Hall"},
-    "Shuttle Landing Platform": {"South": "Grand Training Hall", "West": "Prison Tower"},
-    "Grand Training Hall": {
-        "North": "Shuttle Landing Platform",
-        "South": "Meditation Room",
-        "West": "Vault",
-        "East": "Sith Lords Chambers"
-    },
-    "Meditation Room": {"North": "Grand Training Hall", "East":"Ancient Library"},
-    "Sith Lords Chambers": {"South": "Ancient Library", "West": "Grand Training Hall", "East": "Throne Room"},
-    "Ancient Library": {"North": "Sith Lords Chambers", "West":"Meditation Room"},
-    "Throne Room": {"West": "Sith Lords Chambers"}
-}
+        'Great Hall': {'South': 'Bedroom'},
+        'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
+        'Cellar': {'West': 'Bedroom'}
+    }
